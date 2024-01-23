b3e5b55d0c9cdc311c71014951500fa29cd9a75a
avk1702 <avk1702@bk.ru>
2024-01-23 10:42:11 +0500

    4

---
+++ TeacherView.java
@@ -0,0 +1,18 @@
+package OOP_4;
+
+import java.util.List;
+
+public class TeacherView implements UserView<Teacher> {
+
+    @Override
+    public void sendOnConsole(List<Teacher> list) {
+        for (Teacher teacher : list) {
+            System.out.println(teacher.getTeacherid());
+        }
+    }
+    
+}
+
+
+ 
+
